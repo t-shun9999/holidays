a0af83e1fd24d40aadd63ecb8394b455ce7fc38c
t-shun9999 <exam.zero@gmail.com>
2025-02-06 00:59:46 +0900

    指定年の祝日を取得する処理を追加

---
+++ __tests__/holydays.test.ts
@@ -0,0 +1,31 @@
+import { getHolydays } from '#/holydays';
+
+/**
+ * holydaysテストモジュール
+ */
+describe('getHolydays', () => {
+
+    /**
+     * 
+     */
+    test('指定年の祝日がすべて取得できること', () => {
+        const result = getHolydays(2025);
+
+        expect(result.length).toBe(15);
+        expect(result[0]).toEqual({ date: '2025/01/01', name: '元旦' })
+        expect(result[1]).toEqual({ date: '2025/01/13', name: '成人の日' })
+        expect(result[2]).toEqual({ date: '2025/02/11', name: '建国記念の日' })
+        expect(result[3]).toEqual({ date: '2025/02/24', name: '天皇誕生日（振替）' })
+        expect(result[4]).toEqual({ date: '2025/03/20', name: '春分の日' })
+        expect(result[5]).toEqual({ date: '2025/04/29', name: '昭和の日' })
+        expect(result[6]).toEqual({ date: '2025/05/05', name: 'こどもの日' })
+        expect(result[7]).toEqual({ date: '2025/05/06', name: 'みどりの日（振替）' })
+        expect(result[8]).toEqual({ date: '2025/07/21', name: '海の日' })
+        expect(result[9]).toEqual({ date: '2025/08/11', name: '山の日' })
+        expect(result[10]).toEqual({ date: '2025/09/15', name: '敬老の日' })
+        expect(result[11]).toEqual({ date: '2025/09/23', name: '秋分の日' })
+        expect(result[12]).toEqual({ date: '2025/10/13', name: 'スポーツの日' })
+        expect(result[13]).toEqual({ date: '2025/11/03', name: '文化の日' })
+        expect(result[14]).toEqual({ date: '2025/11/24', name: '勤労感謝の日（振替）' })
+    });
+});
